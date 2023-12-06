133694108b9379de0532a1653af157fb30c5268e
pharell <86664849+Pharell77coder@users.noreply.github.com>
2023-12-06 14:44:06 +0100

    update grid

--- grid.py
+++ grid.py
@@ -1,18 +1,36 @@
 import pygame
+import random
 from constants import *
 
+
 class Square:
-    def __init__(self, pos):
+    def __init__(self, pos, nom):
         self.x = pos[0]
         self.y = pos[1]
         self.margin = 0.5
 
         self.border = 'black'
 
+        self.province = nom
+        self.ideologie = {"communiste": 0, "liberal": 0, "fasciste": 0}
+        self.garnison = random.randint(0, 100)
+        self.population = random.randint(0, 10000)
+        self.ressource = {"electricite": 0, "port": 0, "petrole": 0}
+        self.economie = 0
+
+    def get_pos(self):
+        return self.x, self.y
+
+    def get_info(self):
+        return [self.province, self.ideologie, self.garnison, self.population, self.ressource, self.economie]
+
     def draw(self, screen, color):
-        pygame.draw.rect(screen, self.border, (self.margin + self.x * TILESIZE, self.margin + self.y * TILESIZE, TILESIZE, TILESIZE))
+        pygame.draw.rect(screen, self.border,
+                         (self.margin + self.x * TILESIZE, self.margin + self.y * TILESIZE, TILESIZE, TILESIZE))
         pygame.draw.rect(screen, color,
-                         (self.margin + self.x * TILESIZE + self.margin, self.margin + self.y * TILESIZE + self.margin, TILESIZE - self.margin * 2, TILESIZE - self.margin* 2))
+                         (self.margin + self.x * TILESIZE + self.margin, self.margin + self.y * TILESIZE + self.margin,
+                          TILESIZE - self.margin * 2, TILESIZE - self.margin * 2))
+
 
 class Grid:
     def __init__(self, screen, font, root):
@@ -20,31 +38,45 @@ class Grid:
         self.screen = screen
         self.font = font
         self.root = root
+        self.pos = (0, 0)
 
-    def texte(self, text, pos, color='black', bool=True):
-        menu_text = self.font.render(text, bool, color)
-        menu_rect = menu_text.get_rect(center=(pos[0], pos[1]))
-        self.screen.blit(menu_text, menu_rect)
-
-    def handling(self, event):
-        if event.type == pygame.MOUSEBUTTONUP:
-            mouse_pos = pygame.mouse.get_pos()
-            click_pos = (mouse_pos[0] // TILESIZE, mouse_pos[1] // TILESIZE)
-            print(click_pos)
-        elif event.key == pygame.K_RETURN:
-             self.root.state = "menu"
-
-    def draw(self):
-        self.screen.fill((0, 0, 0))
         col = 7
         row = 9
-
         self.squares = [[(i, j) for i in range(row)] for j in range(col)]
         self.map = []
         for i in self.squares:
             for j in i:
-                self.map.append(Square(j))
+                self.map.append(Square(j, "ville"))
+
+    def texte(self, text, pos, color='black'):
+        menu_text = self.font.render(text, True, color)
+        menu_rect = menu_text.get_rect(top=pos[1])
+        self.screen.blit(menu_text, menu_rect)
+
+    def handling(self, event):
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.pos = pygame.mouse.get_pos()
+
+    def draw(self):
+        global info
+        self.screen.fill((0, 0, 0))
+        click_pos = (self.pos[0] // TILESIZE, self.pos[1] // TILESIZE)
+
         for case in self.map:
-            case.draw(self.screen, (255, 255, 255))
-        pygame.draw.rect(self.screen, 'white', (0, HEIGHT-TILESIZE*2, WIDTH, TILESIZE*2))
-        for i in range(6): self.texte("une variable",(50, 525+(20*i)))
+            if case.get_pos() == click_pos:
+                case.draw(self.screen, (135,206,235))
+                info = case.get_info()
+            else:
+                case.draw(self.screen, (255, 255, 255))
+        pygame.draw.rect(self.screen, 'white', (0, HEIGHT - TILESIZE * 2, WIDTH, TILESIZE * 2))
+        self.texte(f"Nom de La province : {info[0]}", (0, 525))
+        self.texte(
+            f"Ideologie : Communisme : {info[1]["communiste"]}, "
+            f"Libéralisme : {info[1]['liberal']}, Fascisme : {info[1]['fasciste']}",
+            (0, 545))
+        self.texte(f"Garnison : {info[2]}", (0, 565))
+        self.texte(f"Population : {info[3]}", (0, 585))
+        self.texte(f"Ressource : Petrole : {info[4]['petrole']},  "
+            f"Port : {info[4]['port']},  Electricité : {info[4]['electricite']}",
+            (0, 605))
+        self.texte(f"Economie : {info[5]}", (0, 625))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from menu import Menu
 from grid import Grid
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -11,19 +12,18 @@ class Game:
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Un Titre")
         pygame.font.init()
-        self.font = pygame.font.SysFont(None, 24)
+        self.font = pygame.font.SysFont("arial.ttk", 24)
         self.current_time = 0
         self.button_press_time = 0
         self.press = True
-        self.state = "grille"
+        self.state = "grille" #"menu"
         self.running = True
         self.full_screen = False
-
+        self.menu = Menu(self.screen, self.font, self)
+        self.grille = Grid(self.screen, self.font, self)
 
     def run(self):
         self.running = True
-        self.menu = Menu(self.screen, self.font, self)
-        self.grille = Grid(self.screen, self.font, self)
         while self.running:
             for event in pygame.event.get():
 
@@ -31,6 +31,8 @@ class Game:
                     self.running = False
                 elif self.state == "menu" and self.press:
                     self.menu.handling(event)
+                elif self.state == "grille" and self.press:
+                    self.grille.handling(event)
 
                 self.press = False
                 self.button_press_time = pygame.time.get_ticks()
